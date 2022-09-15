2d483ce016891a7c013b018f8148042f88609604
bm <cidhighwind85@hotmail.com>
2022-09-15 13:57:09 +0900

    lambda init

---
+++ TriplesDesTest/Cryptography/RetailMac.cs
@@ -0,0 +1,55 @@
+﻿using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Macs;
+using Org.BouncyCastle.Crypto.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriplesDesTest.Cryptography
+{
+    class RetailMac
+    {
+        public string getMac(byte[] data, byte[] keyBytes)
+        {
+            // From eifd (our "text") = 93 77 45 C2 08 83 A1 BA D1 E0 41 93 72 2A 15 92 37 8F 81 A8 F1 DC 58 91 57 AE B0 F7 54 4F A1 BA
+            // And kmac (the key) = 65 22 B4 E1 71 19 5B B2 18 22 3A 97 6C 04 01 11
+            // we should get 1A D7 FB 6A 33 89 E0 17
+
+            //byte[] keyBytes = kmac; // StringToByteArray(key);
+            //byte[] data = new byte[] { 0x93, 0x77, 0x45, 0xC2, 0x08, 0x83, 0xA1, 0xBA, 0xD1, 0xE0, 0x41, 0x93, 0x72, 0x2A, 0x15, 0x92, 0x37, 0x8F, 0x81, 0xA8, 0xF1, 0xDC, 0x58, 0x91, 0x57, 0xAE, 0xB0, 0xF7, 0x54, 0x4F, 0xA1, 0xBA, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; // Encoding.UTF8.GetBytes(text);
+
+            // NOTE THE PADDING IN THE DATA! 0x80 + whatever is needed to reach a total of 8 bytes. This is padding method 2.
+            // https://en.wikipedia.org/wiki/ISO/IEC_9797-1#Padding, https://stackoverflow.com/questions/6966419/iso-9797-padding-method-2-and-mac-generation-in-java
+            // This is required to get the desired result, similar to how this is done when obtaining the eifd
+
+            DesEngine cipher = new DesEngine();
+            ISO9797Alg3Mac mac = new ISO9797Alg3Mac(cipher);
+
+            KeyParameter keyP = new KeyParameter(keyBytes);
+            mac.Init(keyP);
+            mac.BlockUpdate(data, 0, data.Length);
+
+            byte[] outPut = new byte[8];
+
+            mac.DoFinal(outPut, 0);
+
+            foreach (var item in outPut) { Console.Write("{0:X} ", Convert.ToUInt32(item)); }
+
+            return BytesToHex(outPut);
+        }
+        public byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public string BytesToHex(byte[] bytes)
+        {
+            return String.Concat(Array.ConvertAll(bytes, delegate (byte x) { return x.ToString("X2"); })).ToLower();
+        }
+    }
+}
--- TriplesDesTest/Program.cs
+++ TriplesDesTest/Program.cs
@@ -7,14 +7,19 @@ using Org.BouncyCastle.Crypto.Macs;
 using Org.BouncyCastle.Crypto.Paddings;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Security;
+using TriplesDesTest.Cryptography;
 
 // https://stackoverflow.com/questions/11413576/how-to-implement-triple-des-in-c-sharp-complete-example
 // https://github.com/novotnyllc/bc-csharp
 // https://bouncycastle.org/specifications.html
+
+// lambda resources
+// https://aws.amazon.com/blogs/compute/introducing-the-net-6-runtime-for-aws-lambda/
+// https://github.com/aws/aws-sdk-net/ 
 internal class Program
 {
-    static byte[] cardIdBytes = new byte[] { 0x41, 0x41, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x42, 0x42 };
-    static byte[] cardBytesHashed = new byte[] { 0x65, 0x22, 0xB4, 0xE1, 0x71, 0x19, 0x5B, 0xB2, 0x18, 0x22, 0x3A, 0x97, 0x6C, 0x04, 0x01, 0x11, 0xBD, 0xC4, 0xAA, 0x25 };
+    //static byte[] cardIdBytes = new byte[] { 0x41, 0x41, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x42, 0x42 };
+    //static byte[] cardBytesHashed = new byte[] { 0x65, 0x22, 0xB4, 0xE1, 0x71, 0x19, 0x5B, 0xB2, 0x18, 0x22, 0x3A, 0x97, 0x6C, 0x04, 0x01, 0x11, 0xBD, 0xC4, 0xAA, 0x25 };
 
     // first 16 bytes of the hashed card id
     static byte[] kmac = new byte[] { 0x65, 0x22, 0xB4, 0xE1, 0x71, 0x19, 0x5B, 0xB2, 0x18, 0x22, 0x3A, 0x97, 0x6C, 0x04, 0x01, 0x11 };
@@ -22,6 +27,7 @@ internal class Program
     static byte[] kenc = new byte[] { 0x65, 0x22, 0xB4, 0xE1, 0x71, 0x19, 0x5B, 0xB2, 0x18, 0x22, 0x3A, 0x97, 0x6C, 0x04, 0x01, 0x11 };
     private static void Main(string[] args)
     {
+        var retailMac = new RetailMac();
         Console.WriteLine("Hello, World!");
         // Obtained from ND.IFD, RND.ICC, K.IFD
         var combinedData = new byte[] { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x5A, 0x6E, 0x7E, 0x38, 0x51, 0x62, 0xB7, 0xA3, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F };//, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
@@ -42,7 +48,7 @@ internal class Program
         // Step 5
         var eifd = new byte[] { 0x93, 0x77, 0x45, 0xC2, 0x08, 0x83, 0xA1, 0xBA, 0xD1, 0xE0, 0x41, 0x93, 0x72, 0x2A, 0x15, 0x92, 0x37, 0x8F, 0x81, 0xA8, 0xF1, 0xDC, 0x58, 0x91, 0x57, 0xAE, 0xB0, 0xF7, 0x54, 0x4F, 0xA1, 0xBA, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
         //// Already added the 8 bytes of padding in eifd, from 0x80 and the zeros at the end
-        var mac = getMac(eifd, kmac);
+        var mac = retailMac.getMac(eifd, kmac);
         Console.WriteLine();
 
         //// 6. send a secure message to card, which combines the command bytes + eifd + mac
@@ -58,7 +64,8 @@ internal class Program
         //// M_ICC = 59 38 8F D6 CD 45 24 8B // 90 00 the last 2 are the sw confirmation bytes
 
         //// 7. Verify card authentication MAC (Kmac calculates Retail MAC for E_ICC and compares with M_ICC)
-        verifyResponse(eicc, kmac);
+        //verifyResponse(eicc, kmac);
+        var mac2 = retailMac.getMac(eicc, kmac);
         Console.WriteLine("");
         Console.WriteLine(eicc.Length.ToString());
         // 8. Extract RND.ICC||RND.IFD||K.ICC (TDES decrypt E_ICC with Kenc)
@@ -70,59 +77,59 @@ internal class Program
 
     }
 
-    public static void verifyResponse(byte[] eicc, byte[] keyBytes)
-    {
-        // E_ICC = 58 60 77 5B 4D 03 2C C5 64 BA 20 4B 8E A8 68 F6
-        //         94 A7 4E 74 75 A8 FE F2 40 58 8B DA 1A F4 96 CE
-        //var eicc = new byte[] { 0x58, 0x60, 0x77, 0x5B, 0x4D, 0x03, 0x2C, 0xC5, 0x64, 0xBA, 0x20, 0x4B, 0x8E, 0xA8, 0x68, 0xF6, 0x94, 0xA7, 0x4E, 0x74, 0x75, 0xA8, 0xFE, 0xF2, 0x40, 0x58, 0x8B, 0xDA, 0x1A, 0xF4, 0x96, 0xCE, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-        // this needs padding as well!
+    //public static void verifyResponse(byte[] eicc, byte[] keyBytes)
+    //{
+    //    // E_ICC = 58 60 77 5B 4D 03 2C C5 64 BA 20 4B 8E A8 68 F6
+    //    //         94 A7 4E 74 75 A8 FE F2 40 58 8B DA 1A F4 96 CE
+    //    //var eicc = new byte[] { 0x58, 0x60, 0x77, 0x5B, 0x4D, 0x03, 0x2C, 0xC5, 0x64, 0xBA, 0x20, 0x4B, 0x8E, 0xA8, 0x68, 0xF6, 0x94, 0xA7, 0x4E, 0x74, 0x75, 0xA8, 0xFE, 0xF2, 0x40, 0x58, 0x8B, 0xDA, 0x1A, 0xF4, 0x96, 0xCE, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+    //    // this needs padding as well!
 
-        // M_ICC = 59 38 8F D6 CD 45 24 8B // 90 00 the last 2 are the sw confirmation bytes
+    //    // M_ICC = 59 38 8F D6 CD 45 24 8B // 90 00 the last 2 are the sw confirmation bytes
 
-        // the mac obtained from eicc + kmac should be the same as micc
-        getMac(eicc, keyBytes);
-    }
+    //    // the mac obtained from eicc + kmac should be the same as micc
+    //    getMac(eicc, keyBytes);
+    //}
 
-    public static string getMac(byte[] data, byte[] keyBytes)
-    {
-        // From eifd (our "text") = 93 77 45 C2 08 83 A1 BA D1 E0 41 93 72 2A 15 92 37 8F 81 A8 F1 DC 58 91 57 AE B0 F7 54 4F A1 BA
-        // And kmac (the key) = 65 22 B4 E1 71 19 5B B2 18 22 3A 97 6C 04 01 11
-        // we should get 1A D7 FB 6A 33 89 E0 17
+    //public static string getMac(byte[] data, byte[] keyBytes)
+    //{
+    //    // From eifd (our "text") = 93 77 45 C2 08 83 A1 BA D1 E0 41 93 72 2A 15 92 37 8F 81 A8 F1 DC 58 91 57 AE B0 F7 54 4F A1 BA
+    //    // And kmac (the key) = 65 22 B4 E1 71 19 5B B2 18 22 3A 97 6C 04 01 11
+    //    // we should get 1A D7 FB 6A 33 89 E0 17
 
-        //byte[] keyBytes = kmac; // StringToByteArray(key);
-        //byte[] data = new byte[] { 0x93, 0x77, 0x45, 0xC2, 0x08, 0x83, 0xA1, 0xBA, 0xD1, 0xE0, 0x41, 0x93, 0x72, 0x2A, 0x15, 0x92, 0x37, 0x8F, 0x81, 0xA8, 0xF1, 0xDC, 0x58, 0x91, 0x57, 0xAE, 0xB0, 0xF7, 0x54, 0x4F, 0xA1, 0xBA, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; // Encoding.UTF8.GetBytes(text);
+    //    //byte[] keyBytes = kmac; // StringToByteArray(key);
+    //    //byte[] data = new byte[] { 0x93, 0x77, 0x45, 0xC2, 0x08, 0x83, 0xA1, 0xBA, 0xD1, 0xE0, 0x41, 0x93, 0x72, 0x2A, 0x15, 0x92, 0x37, 0x8F, 0x81, 0xA8, 0xF1, 0xDC, 0x58, 0x91, 0x57, 0xAE, 0xB0, 0xF7, 0x54, 0x4F, 0xA1, 0xBA, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; // Encoding.UTF8.GetBytes(text);
 
-        // NOTE THE PADDING IN THE DATA! 0x80 + whatever is needed to reach a total of 8 bytes. This is padding method 2.
-        // https://en.wikipedia.org/wiki/ISO/IEC_9797-1#Padding, https://stackoverflow.com/questions/6966419/iso-9797-padding-method-2-and-mac-generation-in-java
-        // This is required to get the desired result, similar to how this is done when obtaining the eifd
+    //    // NOTE THE PADDING IN THE DATA! 0x80 + whatever is needed to reach a total of 8 bytes. This is padding method 2.
+    //    // https://en.wikipedia.org/wiki/ISO/IEC_9797-1#Padding, https://stackoverflow.com/questions/6966419/iso-9797-padding-method-2-and-mac-generation-in-java
+    //    // This is required to get the desired result, similar to how this is done when obtaining the eifd
 
-        DesEngine cipher = new DesEngine();
-        ISO9797Alg3Mac mac = new ISO9797Alg3Mac(cipher);
+    //    DesEngine cipher = new DesEngine();
+    //    ISO9797Alg3Mac mac = new ISO9797Alg3Mac(cipher);
 
-        KeyParameter keyP = new KeyParameter(keyBytes);
-        mac.Init(keyP);
-        mac.BlockUpdate(data, 0, data.Length);
+    //    KeyParameter keyP = new KeyParameter(keyBytes);
+    //    mac.Init(keyP);
+    //    mac.BlockUpdate(data, 0, data.Length);
 
-        byte[] outPut = new byte[8];
+    //    byte[] outPut = new byte[8];
 
-        mac.DoFinal(outPut, 0);
+    //    mac.DoFinal(outPut, 0);
 
-        foreach (var item in outPut) { Console.Write("{0:X} ", Convert.ToUInt32(item)); }
+    //    foreach (var item in outPut) { Console.Write("{0:X} ", Convert.ToUInt32(item)); }
 
-        return BytesToHex(outPut);
-    }
-    public static byte[] StringToByteArray(string hex)
-    {
-        return Enumerable.Range(0, hex.Length)
-                         .Where(x => x % 2 == 0)
-                         .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                         .ToArray();
-    }
+    //    return BytesToHex(outPut);
+    //}
+    //public static byte[] StringToByteArray(string hex)
+    //{
+    //    return Enumerable.Range(0, hex.Length)
+    //                     .Where(x => x % 2 == 0)
+    //                     .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+    //                     .ToArray();
+    //}
 
-    public static string BytesToHex(byte[] bytes)
-    {
-        return String.Concat(Array.ConvertAll(bytes, delegate (byte x) { return x.ToString("X2"); })).ToLower();
-    }
+    //public static string BytesToHex(byte[] bytes)
+    //{
+    //    return String.Concat(Array.ConvertAll(bytes, delegate (byte x) { return x.ToString("X2"); })).ToLower();
+    //}
 
     public static byte[] Encrypt(byte[] encryptByteData)
     {
